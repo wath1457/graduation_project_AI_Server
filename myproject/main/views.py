e1acfe90e430c5044cfb0fa01f76f52d2a3f0e66
wath1457 <wath1457@gmail.com>
2022-10-20 03:40:32 +0900

    chatbot local test

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,18 +1,33 @@
+from tkinter.tix import MAX
 from django.shortcuts import render, HttpResponse, redirect
 from main.apps import MainConfig
 
 from .modules.depression_model_init import dm_init
 from .modules.depression_predict import depression_predict
+from .modules.chatbot_model_init import cm_init
+from .modules.chatbot_reply import predict_reply
 
 setting_completed = False
+
+
+# 우울증 분석 변수
 stopwords = None
-tokenizer = None
+depression_tokenizer = None
 model = None
 max_len = None
 
+# 챗봇 변수
+START_TOKEN = None
+END_TOKEN = None
+MAX_LENGTH = None
+chatbot_tokenizer = None
+C_model = None
 
 if setting_completed != True:
-    stopwords, tokenizer, model, max_len = dm_init()
+    # stopwords, depression_tokenizer, model, max_len = dm_init()
+    print('우울증 분석 모델 로드 완료')
+    START_TOKEN, END_TOKEN, MAX_LENGTH, chatbot_tokenizer, C_model = cm_init()
+    print('챗봇 모델 로드 완료')
     setting_completed = True
 else:
     pass
@@ -23,8 +38,38 @@ def index(request):
 def depression(request):
     return HttpResponse(testhtml())
 
+def chatbot(request):
+    predicted_reply = predict_reply('오늘 뭐하지?',START_TOKEN, END_TOKEN, MAX_LENGTH, chatbot_tokenizer, C_model)
+    return HttpResponse(testhtml2())
+
 def testhtml():
         page = f'''
-        <h2>{depression_predict('너무 행복하다', stopwords, tokenizer, model, max_len)}</h2>
+        <h2>{depression_predict('너무 행복하다', stopwords, depression_tokenizer, model, max_len)}</h2>
         '''
         return page
+def testhtml2():
+        page = f'''
+        <h2>결과 : {predict_reply('오늘 뭐하지?', START_TOKEN, END_TOKEN, MAX_LENGTH, chatbot_tokenizer, C_model)}</h2>
+        '''
+        return page
+
+
+# def chat():
+#     score = 0
+#     params = request.get_json()
+#     reply = predict_reply(params['user_chat'])
+#     score = predict_emotion(params['user_chat'])
+#     email = params['user_email']
+
+#     headers = {'Content-Type' : 'application/json; charset=utf-8'}
+#     data = {'email' : email, 'score' : score}
+    
+#     # requests.post(url, data=json.dumps(data), headers=headers)
+#     if score == 1:
+#         print("점수 상승(긍정)")
+#         requests.put(emo_url + 'plus/' + email, data=json.dumps(data), headers = headers)
+#     elif score == -1:
+#         print("점수 하락(부정)")
+#         requests.put(emo_url + 'minus/' + email, data=json.dumps(data), headers = headers)
+        
+#     return jsonify({"reply" : reply})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -6,5 +6,6 @@ from main import views as main_views
 urlpatterns = [
     path('', main_views.index, name="index"),
     path('depression/', main_views.depression),
+    path('chatbot/', main_views.chatbot),
     path('admin/', admin.site.urls)
 ]
